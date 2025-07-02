8c6598044826f8ce5bc556f2558a7320d2835cbf
Nabila431 <jesikamahjong@gmail.com>
2025-07-02 10:00:04 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// js/supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "../config.js";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export default supabase;
